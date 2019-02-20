c6c664319f9c3569cb8c1f51860ce4584e6038fb
noether17 <jawhite@oakland.edu>
2019-02-20 18:03:04 -0500

    add ex1_12

---
+++ ex1_12.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#define IN 1 // inside a word
+#define OUT 0 // outside a word
+
+main()
+{
+	int c, state;
+
+	state = OUT;
+	while ((c = getchar()) != EOF)
+	{
+		if ((c == ' ' || c == '\n' || c == '\t') && state == IN)
+		{
+			putchar('\n');
+			state = OUT;
+		}
+		else
+		{
+			state = IN;
+			putchar(c);
+		}
+	}
+}
